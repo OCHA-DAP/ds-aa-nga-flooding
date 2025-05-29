daa505dd74cb0e31e941abdb7838114e128e26bc
hannahker <hannah.ker96@gmail.com>
2025-05-29 10:25:43 -0700

    save all data to database in single script

--- monitoring/download_glofas.py
+++ monitoring/download_glofas.py
@@ -1,9 +1,15 @@
 from datetime import datetime, timedelta
 
 import ocha_stratus as stratus
+import pandas as pd
+import requests
+import xarray as xr
+from dotenv import load_dotenv
 
-from src.datasources import glofas
+from src.datasources import glofas, grrr
 from src.utils import cds_utils
+
+load_dotenv()
 
 
 def get_blob_name(data_type, date):
@@ -28,13 +34,9 @@ def get_glofas_forecast(
     forecast_blob_name,
     coords,
     issued_date,
-    leadtimes=None,
     keep_local_copy=True,
     overwrite=False,
 ):
-    if not leadtimes:
-        leadtimes = (["24", "48", "72", "96", "120"],)
-
     container = stratus.get_container_client("projects", "dev")
     if (
         container.get_blob_client(forecast_blob_name).exists()
@@ -51,7 +53,13 @@ def get_glofas_forecast(
         "year": [str(issued_date.year)],
         "month": [str(issued_date.month).zfill(2)],
         "day": [str(issued_date.day).zfill(2)],
-        "leadtime_hour": leadtimes,
+        "leadtime_hour": [
+            "24",
+            "48",
+            "72",
+            "96",
+            "120",
+        ],
         "data_format": "grib2",
         "download_format": "unarchived",
         "area": coords,
@@ -100,6 +108,58 @@ def get_glofas_reanalysis(
     )
 
 
+def get_google_forecast(hybas_id, issued_date):
+    res = requests.get(
+        "https://floodforecasting.googleapis.com/v1/gauges:queryGaugeForecasts",  # noqa
+        params={
+            "key": grrr.FLOODS_API_KEY,
+            "gaugeIds": hybas_id,
+            "issuedTimeStart": issued_date.strftime("%Y-%m-%d"),
+        },
+    ).json()
+
+    rows = []
+
+    for forecast in res["forecasts"][hybas_id]["forecasts"]:
+        issued_time = forecast["issuedTime"]
+        gauge_id = forecast["gaugeId"]
+
+        for range_item in forecast["forecastRanges"]:
+            row = {
+                "issued_time": issued_time,
+                "valid_time": range_item["forecastStartTime"],
+                "value": range_item["value"],
+                "src": gauge_id,
+            }
+            rows.append(row)
+
+    df = pd.DataFrame(rows)
+    # In case of multiple forecasts issued from the same day, we want to keep
+    # the one that was issued latest
+    df = df[df.issued_time == df.issued_time.max()]
+    return df
+
+
+def process_glofas(blob_name, data_type):
+    ds = xr.open_dataset(
+        f"temp/{blob_name}",
+        engine="cfgrib",
+        decode_timedelta=True,
+        backend_kwargs={
+            "indexpath": "",
+        },
+    )
+    df = (
+        ds.assign_coords(valid_time=ds["valid_time"] - pd.Timedelta(hours=24))
+        .to_dataframe()
+        .reset_index()
+    )
+    df["valid_time"] = pd.to_datetime(df["valid_time"])
+    df["src"] = data_type
+    df = df.rename(columns={"dis24": "value", "time": "issued_time"})
+    return df[["issued_time", "valid_time", "value", "src"]]
+
+
 if __name__ == "__main__":
     today = datetime.now()
     two_days_ago = today - timedelta(days=2)
@@ -108,5 +168,27 @@ if __name__ == "__main__":
     forecast_blob_name = get_blob_name("forecast", today)
     reanalysis_blob_name = get_blob_name("reanalysis", two_days_ago)
 
-    get_glofas_forecast(forecast_blob_name, coords, today)
-    get_glofas_reanalysis(reanalysis_blob_name, coords, two_days_ago)
+    # Saving raw GloFAS data...
+    get_glofas_forecast(forecast_blob_name, coords, today, overwrite=False)
+    get_glofas_reanalysis(
+        reanalysis_blob_name, coords, two_days_ago, overwrite=False
+    )
+
+    # Get the Glofas dataframes...
+    df_forecast = process_glofas(forecast_blob_name, "glofas_forecast")
+    df_reanalysis = process_glofas(reanalysis_blob_name, "glofas_reanalysis")
+
+    # Getting Google dataframe...
+    df_google = get_google_forecast(grrr.HYBAS_ID, today)
+
+    # Combine and save to database...
+    df_all = pd.concat([df_forecast, df_reanalysis, df_google])
+    engine = stratus.get_engine(stage="dev", write=True)
+    df_all.to_sql(
+        "nga_cerf_flooding",
+        schema="monitoring",
+        con=engine,
+        if_exists="append",
+        index=False,
+        method=stratus.postgres_upsert,
+    )
